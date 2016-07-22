d18edb7482a00a834af8453b66c010e4999573fe
Lance <yuhung1990@gmal.com>
2016-07-22 16:53:42 +0800

    change mouseover to setTimeout

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,10 @@
-$(document).ready(function () {
-	$('body').on('mouseover', function () {
+$(function(){
 		var text = "Hi I'm Lance";
 		var text1="Welcome to my website";
 		var text2="You can find my curriculum vitae <br> and some of my works here"
 		
-		$('.cv').addClass('scroll');
-		$('.cv').html('<h1>'+ text + '<br>' + text1 + '<br>'+ text2+'</h1>');
-		
-	});
-})
+		setTimeout(function(){
+			$('.cv').addClass('scroll');
+			$('.cv').html('<h1>'+ text + '<br>' + text1 + '<br>'+ text2+'</h1>');
+		},2500);
+});
